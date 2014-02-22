6f232f362dafe13d9c42872932cd6eeb13b6fe90
Bruno Costa <bruno.not@hotmail.com>
2014-02-22 10:44:43 -0300

    Reseting the project.

---
+++ SGEWeb/src/managedBean/UserMBean.java
@@ -0,0 +1,12 @@
+package managedBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="userMBean")
+@SessionScoped
+public class UserMBean {
+
+	public UserMBean(){}
+
+}
